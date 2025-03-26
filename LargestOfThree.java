4c8f20f96509ae27a8cfe1adef80482754faea03
harshitclub <harshitclub@gmail.com>
2025-03-26 15:13:34 +0530

    enhancing the largest number program

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -1,9 +1,8 @@
-public class LargestOfThree {
-    public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
-        int num3 = 30;
+import java.util.Scanner;
 
+public class LargestOfThree {
+
+    static void findLargest(int num1, int num2, int num3) {
         if (num1 > num2 && num1 > num3) {
             System.out.println(num1 + " is greater than other two.");
         } else if (num2 > num1 && num2 > num3) {
@@ -11,5 +10,20 @@ public class LargestOfThree {
         } else {
             System.out.println(num3 + " is greater than other two.");
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        findLargest(num1, num2, num3);
+
+        sc.close();
     }
 }
